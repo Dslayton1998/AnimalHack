a45c5fc419c382ac24f856e7ba960660e614aca0
Dslayton1998 <dslayton1998@gmail.com>
2024-06-03 14:42:25 -0500

    first commit

---
+++ Ideas.js
@@ -0,0 +1,52 @@
+/*
+    *AnimalHack 2024
+        ? AnimalHack is a platform that inspires you to think of needs, wants and challenges in interacting with animals. Solutions can take many different forms!
+
+    
+    Expected project topics include, but are not limited to: 
+
+    * Contributing to animal wellbeing 
+
+    * Assisting animal owners 
+
+    * Assisting animal adoptions !
+
+    * Monitoring, conserving and improving the environment around animals 
+
+    * Observing, tracing and protecting wild animals 
+
+    * Breeding, rearing/farming and harvesting animals 
+
+    * Managing nuisance issues with wildlife
+
+    * Addressing animal abuse, cruelty and illegal capture/trade 
+
+ 
+
+Solutions can take many different forms such as apps, games, social platforms, web sites/services, devices, robots, data collection/storage, data analysis/forecasts, data visualization, information retrieval, and 3-dimensional modeling/printing. 
+
+AnimalHack is a “flexible” online hackathon. Participants can work on their projects anytime until the event starts. Each participant/team is required to submit a project presentation title and contact information by 12pm on September 7 (Sat), EST. 
+
+AnimalHack will take place at 1pm on September 8 (Sun), EST. Each participant/team will deliver a project presentation (~10 minutes) online. Various awards are available. A panel of judges will select award winners at the end of the event. 
+
+AnimalHack is hosted by Binnovative, a nonprofit organization in Massachusetts.
+
+We will publish a book that features the projects presented at AnimalHack 2024. Selected participants will be invited to extend their project abstracts and publish them as book chapters. 
+We are planning to publish this book through Amazon Kindle Direct Publishing.
+
+
+JUDGING CRITERIA
+    Impact
+        - How much impact (quality and quantity) can this project have? Does it solve a big problem or a small problem? Will it inspire or help many or a few?
+    Creativity
+        - How creative/innovative is this project? Is it something that hasn't been attempted before, or is it an incremental improvement on something that already exists?
+    Presentation
+        - How well did the team/individual communicate this project? Was the presentation effective in telling the story of the project: why it is important, what challenge it addresses, and how it delivers a solution?
+
+
+* What is this site for, what purpose does it serve?
+* Who will be interacting with the site?
+* What does the tech stack include?
+    -
+* When will be the best time to work on the site?
+*/
