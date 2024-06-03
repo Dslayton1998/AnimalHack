bd0d0411817dfbab534a99ff8db4f2e29e3663ed
Dslayton1998 <dslayton1998@gmail.com>
2024-06-03 16:00:56 -0500

    Initial Planning

--- Ideas.js
+++ Ideas.js
@@ -23,11 +23,14 @@
 
  
 
-Solutions can take many different forms such as apps, games, social platforms, web sites/services, devices, robots, data collection/storage, data analysis/forecasts, data visualization, information retrieval, and 3-dimensional modeling/printing. 
+Solutions can take many different forms such as; 
+apps, games, social platforms, web sites/services, devices, robots, data collection/storage, data analysis/forecasts, data visualization, information retrieval, and 3-dimensional modeling/printing. 
 
-AnimalHack is a “flexible” online hackathon. Participants can work on their projects anytime until the event starts. Each participant/team is required to submit a project presentation title and contact information by 12pm on September 7 (Sat), EST. 
+AnimalHack is a “flexible” online hackathon. Participants can work on their projects anytime until the event starts. 
+Each participant/team is required to submit a project presentation title and contact information by 12pm on September 7 (Sat), EST. 
 
-AnimalHack will take place at 1pm on September 8 (Sun), EST. Each participant/team will deliver a project presentation (~10 minutes) online. Various awards are available. A panel of judges will select award winners at the end of the event. 
+AnimalHack will take place at 1pm on September 8 (Sun), EST. Each participant/team will deliver a project presentation (~10 minutes) online. Various awards are available. 
+A panel of judges will select award winners at the end of the event. 
 
 AnimalHack is hosted by Binnovative, a nonprofit organization in Massachusetts.
 
@@ -41,12 +44,49 @@ JUDGING CRITERIA
     Creativity
         - How creative/innovative is this project? Is it something that hasn't been attempted before, or is it an incremental improvement on something that already exists?
     Presentation
-        - How well did the team/individual communicate this project? Was the presentation effective in telling the story of the project: why it is important, what challenge it addresses, and how it delivers a solution?
+        - How well did the team/individual communicate this project? Was the presentation effective in telling the story of project: 
+            - why it is important?
+            - what challenge it addresses?
+            - and how it delivers a solution?
 
 
 * What is this site for, what purpose does it serve?
+    - Assisting animal adoptions, fit animal personality's to adoption family (if signing a pet up for adoption, fill out a pet personality form)
+
 * Who will be interacting with the site?
+    - Anyone wanting to put their pet up for adoption, anyone who is wanting to adopt a pet
+
 * What does the tech stack include?
-    -
+    - Front: React Native, SocketIO, (Django, TypeScript, Tailwind)
+
+* Legal paperwork for pet adoption?
+    - Include paperwork if applicable (health/ vac's records)
+    - Care requirements (from previous owner)
+
 * When will be the best time to work on the site?
+    - Wed & Thurs (get together days) 
+    - 2:00 - 5:00ish
+
+* Name?
+    -
+
+
+? MVP Features List ?
+1. Pet Sign Up / Pet profiles (Views of profile, adoption status)
+2. Pet Adoption form
+3. Messenger (Spam prevention)
+4. User Profiles (Pet history, Location)
+5. Filter pets
+6. Favorite a pet
+...
+
+? Style Ideas ?
+* Random pet profiles displayed
+...
+
+
+* Bonus *
+- Email verification
+- OAuth 
+- On the App Store
 */
